b35f2f4dcdd966131bb9c43867056b98ddf86727
Piyush <piyushpuniya00@gmail.com>
2021-08-17 18:51:11 +0530

    firebase web push noti in web

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = process.env.PORT;
-// const port = 5000;
+// const port = process.env.PORT;
+const port = 5000;
 const fs = require("fs");
 const path = require("path");
 const mongoose = require('mongoose')
 var bodyParser = require('body-parser');
 const firebase = require("firebase-admin");
 const serviceAccount = require("./service-account-file.json");
+const queryString = require("query-string");
+const axios = require("axios");
+var Session = require('express-session');
+var google = require('googleapis');
+const { OAuth2Client } = require('google-auth-library');
+
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 
 app.use("/static", express.static("static"));
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +29,21 @@ firebase.initializeApp({
     databaseURL: "https://jovial-branch-295110-default-rtdb.firebaseio.com/"
 });
 
+
+const stringifiedParams = queryString.stringify({
+    client_id: "92463015553-fv6fqpch3hifsd9tjjda92pq0n23nbfm.apps.googleusercontent.com",
+    redirect_uri: 'http://localhost:5000/auth',
+    scope: [
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile',
+    ].join(' '), // space seperated string
+    response_type: 'code',
+    access_type: 'offline',
+    prompt: 'consent',
+});
+
+console.log(stringifiedParams)
+
 mongoose.connect("mongodb+srv://noti_token:piyush2001@notitoken.flzpe.mongodb.net/test", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
@@ -31,7 +55,8 @@ db.once('open', function () {
 
 });
 const noti_token = new mongoose.Schema({
-
+    usermail: String,
+    username: String,
     device_token: String,
 
 });
@@ -43,10 +68,49 @@ noti_token.methods.speak = function () {//this is used in case you wanna have ha
 
 
 const c_noti_token = mongoose.model('noti_token', noti_token);
-app.get('/', (req, res) => {
+app.get('/generateToken', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
 
+app.get('/', (req, res) => {
+    var params = { "data": stringifiedParams }
+    res.status(200).render('index', params);
+});
+
+app.post('/auth', (req, res) => {
+    var user_idtoken = req.body.useridtoken;
+    console.log(user_idtoken);
+    //or send post or get at https://oauth2.googleapis.com/tokeninfo?id_token=client_idtoken to verify user
+    const client = new OAuth2Client("92463015553-fv6fqpch3hifsd9tjjda92pq0n23nbfm.apps.googleusercontent.com");
+    async function verify() {
+        console.log("verifying");
+        const ticket = await client.verifyIdToken({
+            idToken: user_idtoken,
+            audience: "92463015553-fv6fqpch3hifsd9tjjda92pq0n23nbfm.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+
+        console.log(payload['given_name'])
+        const user = new c_noti_token({
+            username: payload.given_name,
+            usermail: payload.email
+        });
+
+        // Inserts a new document with `name = 'Will Riker'` and
+        // `rank = 'Commander'`
+        user.save();
+        // If request specified a G Suite domain:
+        // const domain = payload['hd'];
+    }
+    verify().catch(console.error);
+
+    res.status(200).render('admin');
+
+
+});
 
 
 app.get('/firebase-messaging-sw.js', (req, res) => {        //get requests to the root ("/") will route here
@@ -72,7 +136,7 @@ app.get('/admin', (req, res) => {
     c_noti_token.find({}, function (err, someValue) {
         if (err) console.log(err);
         const params = { 'alltokens': JSON.stringify(someValue) };
-        console.log(params);
+        // console.log(params);
         res.status(200).render('admin', params);
 
     });
@@ -86,7 +150,7 @@ app.post('/admin', (req, res) => {
     // sendnoti(clientToken, res);
     const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
- 
+
 
     wait(4 * 1000).then(() => {
         console.log("waited for 4 seconds");
@@ -103,11 +167,8 @@ app.post('/admin', (req, res) => {
 
 });
 
-function sendres(res) {
-    res.send("sent")
-}
 
-// sendnoti("jlkj", "jvhj")
+
 // The Firebase token of the device which will get the notification
 // It can be a string or an array of strings
 function sendnoti(clientToken) {
@@ -115,22 +176,17 @@ function sendnoti(clientToken) {
     // const firebaseToken = 'cCwYg6Lclvt5s9TKuFIMWV:APA91bHa9eT74fkeP4LVydr1f6r3YkzwXs8D7UcAq1i7BZahtlFXAxVqkCR890qFCrkIcFnch1VNNVcdOACCl9F-iC4oudGqElY7Q2SnOgAXbYSMkH61wbYtVhXX8VZ7GIpTZGIB_VwD';
     const firebaseToken = clientToken;
 
-    // firebase.initializeApp({
-    //     credential: firebase.credential.cert(serviceAccount),
-    //     databaseURL: "https://jovial-branch-295110-default-rtdb.firebaseio.com/"
-    // });
 
     const payload = {
         notification: {
             title: 'Notification Title',
-            subtitle : "test subtitle",
+            subtitle: "test subtitle",
             body: 'This is an my noti',
             image: "https://solarianprogrammer.com/images/2015/05/08/circles.jpg",
             click_action: "/"
 
-
         },
-       
+
     };
 
     const options = {
@@ -143,6 +199,12 @@ function sendnoti(clientToken) {
     // res.send("sending noti");      
 
 }
+
+// function getOAuthClient() {
+//     return new OAuth2("92463015553-fv6fqpch3hifsd9tjjda92pq0n23nbfm.apps.googleusercontent.com", "LAhUoyQIAuHlKt6HsPV1E8jr", "http://localhost:5000/auth");
+// }
+
+
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`);
--- static/admin_handler.js
+++ static/admin_handler.js
@@ -2,16 +2,18 @@ var alltokens = document.getElementById("token_data").getAttribute("data")
 data = JSON.parse(alltokens);
 console.log(data);
 
-for(x in data){
-    createNotiBtn(data[x]["device_token"]);
-    console.log(data[x]["device_token"]);
+for (x in data) {
+    if (data[x]["device_token"]) {
+        createNotiBtn(data[x]["device_token"]);
+    }
+    // console.log(data[x]["username"]);
 }
 
-function createNotiBtn(token){
+function createNotiBtn(token) {
     var noti_form = document.createElement("form");
     noti_form.method = "POST";
     noti_form.action = "/admin";
-    
+
     var noti_token = document.createElement("input");
     noti_token.type = "hidden";
     noti_token.value = token;
@@ -28,3 +30,59 @@ function createNotiBtn(token){
     document.getElementById("noti_div").appendChild(noti_form);
 
 }
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+
+    var savedbbtn = document.getElementById("dbsave");
+    savedbbtn.style.display = "block";
+    console.log("Savebtn display");
+    savedbbtn.onclick = function () {
+        savetodb(this, id_token);
+    }
+    document.getElementById("signOut").style.display = "block";
+
+}
+
+function savetodb(btn, id_token) {
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    var theUrl = "/auth";
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.onload = function () {
+        console.log(xmlhttp.responseURL); // http://example.com/test
+        btn.innerHTML = "Saved";
+    };
+
+    xmlhttp.onerror = function () {
+        console.log("** An error occurred during the transaction");
+        btn.innerHTML = "TRy Again - Save in db";
+
+    };
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({
+        useridtoken: id_token
+    }));
+}
+console.log("test");
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    var savedbbtn = document.getElementById("dbsave");
+    savedbbtn.style.display = "block";
+    document.getElementById("signOut").style.display = "none";
+
+}
+
+
