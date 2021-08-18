9cac2e4eb771880b60e336f77d19a2a2ea0c507d
Piyush <piyushpuniya00@gmail.com>
2021-08-18 15:09:46 +0530

    webpush and google auth

--- routes/googlelogin.js
+++ routes/googlelogin.js
@@ -16,40 +16,41 @@ router.route("/").get((req, res) => {
 
 });
 
-router.route("/").post((req, res) => {
+// router.route("/").post((req, res) => {
 
-    var user_idtoken = req.body.useridtoken;
-    console.log(user_idtoken);
-    //or send post or get at https://oauth2.googleapis.com/tokeninfo?id_token=client_idtoken to verify user
-    const client = new OAuth2Client("92463015553-fv6fqpch3hifsd9tjjda92pq0n23nbfm.apps.googleusercontent.com");
-    async function verify() {
-        console.log("verifying");
-        const ticket = await client.verifyIdToken({
-            idToken: user_idtoken,
-            audience: "92463015553-fv6fqpch3hifsd9tjjda92pq0n23nbfm.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
-            // Or, if multiple clients access the backend:
-            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-        });
-        const payload = ticket.getPayload();
-        const userid = payload['sub'];
+//     var user_idtoken = req.body.useridtoken;
+//     console.log(user_idtoken);
+//     //or send post or get at https://oauth2.googleapis.com/tokeninfo?id_token=client_idtoken to verify user
+//     const client = new OAuth2Client("92463015553-fv6fqpch3hifsd9tjjda92pq0n23nbfm.apps.googleusercontent.com");
+//     async function verify() {
+//         console.log("verifying");
+//         const ticket = await client.verifyIdToken({
+//             idToken: user_idtoken,
+//             audience: "92463015553-fv6fqpch3hifsd9tjjda92pq0n23nbfm.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
+//             // Or, if multiple clients access the backend:
+//             //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+//         });
+//         const payload = ticket.getPayload();
+//         const userid = payload['sub'];
 
-        console.log(payload['given_name'])
-        const user = new c_noti_token({
-            username: payload.given_name,
-            usermail: payload.email
-        });
+//         console.log(payload['given_name'])
+//         const user = new c_noti_token({
+//             username: payload.given_name,
+//             usermail: payload.email
+//         });
+        
 
-        // Inserts a new document with `name = 'Will Riker'` and
-        // `rank = 'Commander'`
-        user.save();
-        // If request specified a G Suite domain:
-        // const domain = payload['hd'];
-    }
-    verify().catch(console.error);
+//         // Inserts a new document with `name = 'Will Riker'` and
+//         // `rank = 'Commander'`
+//         user.save();
+//         // If request specified a G Suite domain:
+//         // const domain = payload['hd'];
+//     }
+//     verify().catch(console.error);
 
-    res.status(200).render('admin');
+//     res.status(200).render('admin');
 
-});
+// });
 
 
 router.route('/auth').post((req, res) => {
@@ -75,9 +76,22 @@ router.route('/auth').post((req, res) => {
             usermail: payload.email
         });
 
+        u_mail = payload.email;
+
+        c_noti_token.find({usermail : u_mail}, function (err, someValue) {
+            if (err) console.log(err);
+            if(someValue[0] == undefined){
+                
+                user.save();
+                console.log("doesn't exists");
+
+            }else{
+                console.log("already exists");
+            }
+        });
+
         // Inserts a new document with `name = 'Will Riker'` and
         // `rank = 'Commander'`
-        user.save();
         // If request specified a G Suite domain:
         // const domain = payload['hd'];
     }
